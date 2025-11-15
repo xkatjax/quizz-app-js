8a2a239815ec48c2cfc0026fc3c99aa100b2dfee
Katarzyna Brejna <kb.brejna@gmail.com>
2025-11-15 19:16:06 +0100

    Add quiz results summary

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let currentQuestion = 0;
 let score = 0;
+let answerHistory = [];
 
 function loadQuestion() {
   const questionElement = document.querySelector("#question");
@@ -29,6 +30,13 @@ function checkAnswer(selectedIndex, button) {
   const current = questions[currentQuestion];
   const isCorrect = selectedIndex === current.correct;
 
+  answerHistory.push({
+    question: current.question,
+    correctAnswer: current.answers[current.correct],
+    userAnswer: current.answers[selectedIndex],
+    isCorrect: isCorrect,
+  });
+
   if (isCorrect) {
     score++;
     button.classList.add("quiz-app__answer--correct");
@@ -54,13 +62,48 @@ function checkAnswer(selectedIndex, button) {
   }, 1000);
 }
 
+function escapeHTML(str) {
+  return str.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
 function showResult() {
   const wrapper = document.querySelector(".quiz-app");
-  const percentage = (score * 100) / questions.length;
+  const percentage = Math.round((score * 100) / questions.length);
+
+  const resultsList = answerHistory
+    .map((entry, index) => {
+      return `
+    <li>
+    <strong>${entry.question}</strong>
+          Twoja odpowiedź: 
+            <span style="color:${
+              entry.isCorrect ? "rgb(0, 120, 127)" : "rgb(242, 91, 65)"
+            };">
+              ${escapeHTML(entry.userAnswer)}
+            </span></br>
+          Poprawna odpowiedź: <span style="color: rgb(0, 120, 127)"> ${escapeHTML(
+            entry.correctAnswer
+          )}</span>
+    </li>
+    `;
+    })
+    .join("");
+
+  console.log(resultsList);
+
   wrapper.innerHTML = `
+  <div class="quiz-app__result-summary">
     <h2 class="quiz-app__result-title">Koniec quizu!</h2>
-    <p class="quiz-app__result-score">Twój wynik: ${score} / ${questions.length}</p>
+    <p class="quiz-app__result-summary-score">Twój wynik: ${score} / ${questions.length}</p>
     <h3>Poprawnie odpowiedziałeś na ${percentage}%</h3>
+    <hr class="divider" />
+    <div>
+      <h4>Podsumowanie:</h4>
+      <ol> 
+        ${resultsList}
+      </ol>
+      </div>
+  </div>
   `;
 }
 
